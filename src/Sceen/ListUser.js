b6756c16026dac3ca980a37b567b875330629a2d
TranQuocHuuSOS <0tranquochuu02003@gmail.com>
2024-01-13 11:22:30 +0700

    Repos of management

---
+++ src/Sceen/ListUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListUser extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ListUser;
